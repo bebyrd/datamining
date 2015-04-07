1496bd670f10c0df0fe76b75119ac381ea1b1271
Benjamin Byrd <bebyrd@gmail.com>
2015-04-07 18:42:27 -0400

    Divide by zero errors still present!

--- driverCSV.py
+++ driverCSV.py
@@ -14,28 +14,28 @@ from subprocess import *
 fileReader1 = 'FileReader1.py'
 assert os.path.exists(fileReader1),'FileReader1.py not found'
 
-#trip_pathname = sys.argv[1]
-#assert os.path.exists(trip_pathname)
+trip_pathname = sys.argv[1]
+assert os.path.exists(trip_pathname)
 
-trip_pathname = '/Users/bebyrd/Documents/gsu/drivers/15'
+#trip_pathname = '/Users/bebyrd/Documents/gsu/drivers/15'
 #print trip_pathname
 
-#drivers = glob.glob(trip_pathname+'/*')
-#print drivers
+drivers = glob.glob(trip_pathname+'/*')
+print drivers
+for driver in drivers:
+    regex = re.compile(r'drivers\d+')
+    regex2 = re.compile(r'\d+')
+    driverNum = regex2.search(driver).group(0)
+    trips = glob.glob(driver+'/*')
+    #print trips
 
 
-trips = glob.glob(trip_pathname+'/*')
-#print trips
-
-regex = re.compile(r'drivers\d+')
-regex2 = re.compile(r'\d+')
-outnum = regex2.search(trip_pathname).group(0)
-print outnum
-f = open(outnum+'.csv','w')
-for trip in trips:
-    print trip
-    cmd = 'python %s %s' % (fileReader1,trip)
-    output = Popen(cmd, shell=True, stdout=PIPE).communicate()
-    print output
-    #f.write(output+'\n')
-f.close()
+    print driverNum
+    f = open(driverNum+'.csv','w')
+    for trip in trips:
+        #print trip
+        cmd = 'python %s %s' % (fileReader1,trip)
+        output = Popen(cmd, shell=True, stdout=PIPE).communicate()
+        #print output
+        f.write(output[0])
+    f.close()
